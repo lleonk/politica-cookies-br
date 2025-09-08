6f227f6757a76e26eacd40cb75f965593a0754ff
d805696 <rpioli@sf.prefeitura.sp.gov.br>
2025-09-08 14:41:34 -0300

    Inclui os termos de uso de dados de SF

--- politicacookies/src/templates/manage-template.ts
+++ politicacookies/src/templates/manage-template.ts
@@ -4,7 +4,9 @@ export default `
         <h3>Preferências de Cookies</h3>
         <button type="button" data-action="close" class="close-button">&#10005;</button>
     </div>
-
+    
+    <p>Para mais informações consulte o <strong><a target="_blank" href="https://legislacao.prefeitura.sp.gov.br/leis/portaria-secretaria-municipal-da-fazenda-sf-77-de-23-de-abril-de-2021">Aviso de Privacidade e os Termos de Uso</a></strong> estabelecidos pela Secretaria Municipal de Fazenda de São Paulo.</p>
+                        
     <label>
         <input type="checkbox" name="c1" checked disabled>
         Cookies estritamente necessários (sempre ativos)
@@ -29,6 +31,14 @@ export default `
     </label>
     <p>Usados para exibir anúncios relevantes com base nos seus interesses. Também ajudam a medir a eficácia das campanhas publicitárias e limitar a repetição de anúncios.</p>
     
+    <p>Para gerenciar os cookies diretamente em seu navegador recomendamos a leitura dos seguintes links:</p>
+    <ul>
+        <li><a target="_blank" href="https://support.google.com/chrome/answer/95647?hl=pt-BR&co=GENIE.Platform%3DDesktop#zippy=%2Cexcluir-cookies-de-um-site">Chrome</a></li>
+        <li><a target="_blank" href="https://support.microsoft.com/pt-br/windows/gerencie-cookies-no-microsoft-edge-exibir-permitir-bloquear-excluir-e-usar-168dab11-0753-043d-7c16-ede5947fc64d">Microsoft Edge</a></li>
+        <li><a target="_blank" href="https://support.mozilla.org/pt-BR/kb/limpe-cookies-e-dados-de-sites-no-firefox">Firefox</a></li>
+        <li><a target="_blank" href="https://support.apple.com/pt-br/105082">Safari</a></li>
+    </ul>
+                            
     <div>
         <button type="button" data-action="accept">ACEITAR SELECIONADOS</button>
         <button type="button" data-action="close" class="secondary">FECHAR</button>
